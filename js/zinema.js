b70a155923e2a8b9fae70d9b4b14835db80959c2
SageshiSai <saikonogameryt@gmail.com>
2023-10-03 12:26:54 +0200

    Completo sin JSON

--- js/zinema.js
+++ js/zinema.js
@@ -7,64 +7,73 @@ numsala = 0;
 var pelis = document.querySelectorAll( ".pelis" );
 var pelisCartelera = [ "IMG/mechanic.jpg" , "IMG/unmonstruo.jpg" , "IMG/missperegrine.jpg" , "IMG/ozzy.jpg" ];
 var posiArray= 0;
+
 pelis.forEach( pelisid => {
     pelisid.src = pelisCartelera[ posiArray ];
     posiArray++;
 });
 
-var imagetext = document.querySelectorAll();
 
+const contenedorSalas = document.getElementById( "cartelera" );
 
-function verDatos(numsala) {
-  //var elementos = document.querySelectorAll(".sala1");
-    var elementos = document.getElementById("sala" + numsala);
-    for (var i = 1; i <= 4; i++) {
-    // Cambia el 3 por el número máximo de salas que tengas
-    var id = "sala" + i;
-    var elemento = document.getElementById(id);
+contenedorSalas.addEventListener( "click", function( event ){
 
-    if (elemento) {
-        elemento.style.display = "none";
+    if(event.target.classList.contains( "pelis" )) {
+
+        const idPeliculas = event.target.id;
+        numsala = idPeliculas - 1;
+
+        var elementos = document.getElementById( "sala" + idPeliculas);
+
+        for ( var i = 1; i <= 4; i++ ) {
+            
+            var id = "sala" + i;
+            var elemento = document.getElementById( id );
+        
+            if ( elemento ) {
+                elemento.style.display = "none";
+            }
+        }
+
+        elementos.style.display = "block";
+        document.getElementById( "textoSalaButaca" ).textContent = "TOTAL BUTACAS " + TotalButacas[ idPeliculas - 1 ] + " //Butacas Libres " + ButacasLibres[ idPeliculas - 1 ];
+
+        document.getElementById( "textoSalaButaca" ).style.display = "block";
+        document.getElementById( "compraentradas" ).style.display = "block";
     }
-    }
-    elementos.style.display = "block";
-    document.getElementById("textoSalaButaca").textContent =
-    "TOTAL BUTACAS " +
-    TotalButacas[numsala - 1] +
-    " //Butacas Libres " +
-    ButacasLibres[numsala - 1];
-    document.getElementById("textoSalaButaca").style.display = "block";
-    document.getElementById("compraentradas").style.display = "block";
-}
+});
+
+const num = document.getElementById( "numeroent" );
+const bono = document.getElementById( "tipo" );
+
+num.addEventListener( "input", Calcular );
+bono.addEventListener( "input", Calcular );
 
 function Calcular() {
-    var num = document.getElementById("numeroent").value;
-    var bono = document.getElementById("tipo").value;
-
-  var resultado = num * bono;
-    document.getElementById("precio").value = bono;
-    document.getElementById("total").value = resultado;
+    var nume = num.value;
+    var bono = document.getElementById( "tipo" ).value;
+    var resultado = nume * bono;
+    document.getElementById( "precio" ).value = bono;
+    document.getElementById( "total" ).value = resultado;
 }
 
+var btnComprar = document.getElementById( "comprar" );
 
+btnComprar.addEventListener( "click", (e) => {
+    var index = parseInt(document.getElementById( "numeroent" ).value);
 
-var btnComprar = document.getElementById("comprar");
-btnComprar.addEventListener("click", (e) => {
-    console.log(e.target.textContent);
-    var index = parseInt(document.getElementById("numeroent").value);
+    if ( ButacasLibres[ numsala ] > index ) {
 
-    if (ButacasLibres[numsala] > index) {
-    ButacasLibres[numsala] = ButacasLibres[numsala] - index;
+    ButacasLibres[ numsala ] = ButacasLibres[ numsala ] - index;
+
     alert(
         "Se han comprado las entradas correctamente | quedan un total de: " +
-        ButacasLibres[numsala]
+        ButacasLibres[ numsala ]
     );
-    document.getElementById("textoSalaButaca").textContent =
-    "TOTAL BUTACAS " +
-    TotalButacas[numsala - 1] +
-    " //Butacas Libres " +
-    ButacasLibres[numsala - 1];
+
+    document.getElementById( "textoSalaButaca" ).textContent = "TOTAL BUTACAS " + TotalButacas[ numsala ] + " //Butacas Libres " + ButacasLibres[ numsala ];
+
 }   else {
-    alert("No hay suficientes butacas");
+    alert( "No hay suficientes butacas" );
     }
 });
